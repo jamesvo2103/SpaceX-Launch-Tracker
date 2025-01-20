a142a236f494c6f1d80bff4906792fddc47c1b86
jamesvo2103 <vongochieu2103@gmail.com>
2025-01-20 01:11:03 -0500

    styling

--- spacex_tracker/views.py
+++ spacex_tracker/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Launch, Rocket, Payload
+
 # Create your views here.
 def index(request):
       return render(request, "spaceXlaunchTracker/index.html")
 
 def about(request):
       return render(request, "spaceXlaunchTracker/about.html")
+
+#Launches information
+def launches(request):
+    launch_url = "https://api.spacexdata.com/v5/launches/latest"
+    launches = request.get(launch_url).json()
+    return render(request, "spaceXlaunchTracker/launches.html", {
+        "launches": launches
+    })
